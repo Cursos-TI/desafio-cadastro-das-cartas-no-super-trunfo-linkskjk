759e1a45c1e5c651a941a99a2fdab2752e7592bd
linkskjk <lincolngab455@gmail.com>
2025-04-29 10:53:49 -0400

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,21 +1,30 @@
 #include <stdio.h>
 int main () {
+    //declarando dados básicos;
     char estado, estado1;
     char codigo[50], codigo1[50];
     char cidade[50], cidade1[50];
     int pop, pop1;
-    float area, area1, pib, pib1, pontos, pontos1;
+    float area, area1, pib, pib1;
+    int pontos, pontos1;
+
+    //declarando cálculos;
+    float densidade = pop / area;
+    float densidade1 = pop1 / area1;
+
+    float capita = pib / pop;
+    float capita1 = pib1 / pop1;
 
     //Carta 1
-    printf("Selecione um estado com a letra de A a H: ");
+    printf("Diga a letra de um estado de A a H: ");
     scanf("%c", &estado);
     fflush(stdin);
 
-    printf("Adicione esta mesma letra + um numero de 01 a 04: ");
+    printf("Digite esta mesma letra + um numero de 01 a 04: ");
     scanf("%s", &codigo);
     fflush(stdin);
 
-    printf("Escolha o nome de uma cidade deste estado: ");
+    printf("Digite o nome de uma cidade deste estado: ");
     scanf("%s", &cidade);
     fflush(stdin);
 
@@ -32,19 +41,19 @@ int main () {
     fflush(stdin);
 
     printf("Informe o numero de pontos turisticos da cidade: ");
-    scanf("%f", &pontos);
+    scanf("%d", &pontos);
     fflush(stdin);
 
     //Carta 2
-    printf("\n\nSelecione um novo estado com a letra de A a H: ");
+    printf("\n\nDiga a letra de um estado de A a H: ");
     scanf("%c", &estado1);
     fflush(stdin);
 
-    printf("Adicione esta mesma letra + um numero de 01 a 04: ");
+    printf("Digite esta mesma letra + um numero de 01 a 04: ");
     scanf("%s", &codigo1);
     fflush(stdin);
 
-    printf("Escolha o nome de uma cidade deste estado: ");
+    printf("Digite o nome de uma cidade deste estado: ");
     scanf("%s", &cidade1);
     fflush(stdin);
 
@@ -61,25 +70,28 @@ int main () {
     fflush(stdin);
 
     printf("Informe o numero de pontos turisticos da cidade: ");
-    scanf("%f", &pontos1);
+    scanf("%d", &pontos1);
     fflush(stdin);
-//Print da Carta 1
+
     printf("\nCarta 1:\nEstado: %c\n", estado);
     printf("Codigo da carta: %s\n", codigo);
     printf("Cidade: %s\n", cidade);
     printf("Numero de habitantes: %d\n", pop);
-    printf("Área(km²): %fkm²\n", area);
-    printf("PIB: %f\n", pib);
-    printf("Pontos turisiticos: %f\n", pontos);
+    printf("Área(km²): %.2fkm²\n", area);
+    printf("PIB: %.2f\n", pib);
+    printf("Pontos turisiticos: %d\n", pontos);
+    printf("Densidade populacional: %.2fhab/km²\n", densidade);
+    printf("PIB per capita: %.2f reais\n", capita);
 
-//Print da Carta 2
     printf("\nCarta 2: \nEstado: %c\n", estado1);
     printf("Codigo da carta: %s\n", codigo1);
     printf("Cidade: %s\n", cidade1);
     printf("Numero de habitantes: %d\n", pop1);
-    printf("Área(km²): %fkm²\n", area1);
-    printf("PIB: %f\n", pib1);
-    printf("Pontos turisiticos: %f\n", pontos1);
-
+    printf("Área(km²): %.2fkm²\n", area1);
+    printf("PIB: %.2f\n", pib1);
+    printf("Pontos turisiticos: %d\n", pontos1);
+    printf("Densidade populacional: %.2fhab/km²\n", densidade1);
+    printf("PIB per capita: %.2f reais\n", capita1);
+    
     return 0;
 }
